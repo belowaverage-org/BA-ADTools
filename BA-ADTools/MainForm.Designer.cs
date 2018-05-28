0e801d6e14bdc3c83de1c91b33ced7ebc705bde9
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-05-28 17:51:41 -0400

    Add project files.

---
+++ BA-ADTools/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace BAADTools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.readButton = new System.Windows.Forms.Button();
+            this.filterBox = new System.Windows.Forms.TextBox();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lbCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusSpace = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.statusBar.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(225, 316);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 3;
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(0, 0);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 8;
+            // 
+            // filterBox
+            // 
+            this.filterBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filterBox.Location = new System.Drawing.Point(12, 38);
+            this.filterBox.Name = "filterBox";
+            this.filterBox.Size = new System.Drawing.Size(225, 20);
+            this.filterBox.TabIndex = 5;
+            this.filterBox.TextChanged += new System.EventHandler(this.filterBox_TextChanged);
+            this.filterBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.filterBox_KeyPress);
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lbCount,
+            this.statusSpace,
+            this.progressBar});
+            this.statusBar.Location = new System.Drawing.Point(0, 405);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(249, 22);
+            this.statusBar.TabIndex = 6;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(43, 17);
+            this.toolStripStatusLabel1.Text = "Count:";
+            // 
+            // lbCount
+            // 
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(13, 17);
+            this.lbCount.Text = "0";
+            // 
+            // statusSpace
+            // 
+            this.statusSpace.Name = "statusSpace";
+            this.statusSpace.Size = new System.Drawing.Size(76, 17);
+            this.statusSpace.Spring = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 16);
+            this.progressBar.Step = 1;
+            this.progressBar.Value = 50;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripComboBox1});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(249, 27);
+            this.menuStrip.TabIndex = 7;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.printToolStripMenuItem,
+            this.loginToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 23);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(131, 6);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.printToolStripMenuItem.Text = "Print...";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.loginToolStripMenuItem.Text = "Login...";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Server Mode (Default)",
+            "Local Mode"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.Text = "Filter Mode";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 427);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.filterBox);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.listBox1);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "BA-ADTools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.TextBox filterBox;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lbCount;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.ToolStripStatusLabel statusSpace;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+    }
+}
+
---
+++ BA-ADTools/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.Diagnostics;
+
+namespace BAADTools
+{
+    public partial class MainForm : Form
+    {
+        public List<string> stResults = new List<string>();
+        public List<string> nmResults = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //readButton.PerformClick();
+        }
+
+        public void loadResults()
+        {
+            lbCount.Text = "0";
+            progressBar.Style = ProgressBarStyle.Marquee;
+            DirectorySearcher dsearch = new DirectorySearcher();
+            dsearch.PropertiesToLoad.Add("DisplayName");
+            dsearch.Asynchronous = true;
+            dsearch.Filter = "(&(objectClass=user)(!(objectClass=computer)))";
+            dsearch.CacheResults = false;
+            SearchResultCollection results = dsearch.FindAll();
+            stResults.Clear();
+            nmResults.Clear();
+            foreach (SearchResult result in results)
+            {
+                stResults.Add(result.Path);
+                DirectoryEntry entry = result.GetDirectoryEntry();
+                nmResults.Add(entry.Properties["CN"].Value.ToString());
+                lbCount.Text = (int.Parse(lbCount.Text) + 1).ToString();
+                statusBar.Update();
+            }
+            drawResults();
+            progressBar.Style = ProgressBarStyle.Blocks;
+        }
+
+        private void filterBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (e.KeyChar == char.Parse("\r"))
+            //{
+             
+            //}
+        }
+
+        public void drawResults(string filter = "")
+        {
+            listBox1.Hide();
+            listBox1.Items.Clear();
+            lbCount.Text = "0";
+            foreach (string result in nmResults)
+            {
+                if (filter.Equals("") || result.ToLower().Contains(filter.ToLower()))
+                {
+                    lbCount.Text = (int.Parse(lbCount.Text) + 1).ToString();
+                    statusBar.Update();
+                    listBox1.Items.Add(result);
+                }
+            }
+            listBox1.Show();
+        }
+
+        private void filterBox_TextChanged(object sender, EventArgs e)
+        {
+            drawResults(filterBox.Text);
+        }
+    }
+}
